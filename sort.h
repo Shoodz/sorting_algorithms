d6020f5d36576978f7f7c30f43cdf402593f2013
shahd <shoodyomer22214@gmail.com>
2024-01-19 12:56:29 -0800

    B

--- sort.h
+++ sort.h
@@ -21,8 +21,8 @@ typedef struct listint_s
 
 
 /* print */
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 
